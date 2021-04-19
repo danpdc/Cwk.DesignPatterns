09cf4a112f8e411e44ca8bf4708a1527fd590ff3
Dan Patrascu-Baba <dan@danpatrascu.com>
2021-04-19 21:05:09 +0300

    Add singleton design pattern

---
+++ Cwk.DesignPatterns/Cwk.DesignPatterns.Singleton/LoadBalancer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cwk.DesignPatterns.Singleton
+{
+    public class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private int _requestCount = 0;
+
+        private LoadBalancer()
+        {
+            _servers.Add("Server 1");
+            _servers.Add("Server 2");
+            _servers.Add("Server 3");
+            _servers.Add("Server 4");
+            _servers.Add("Server 5");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (_instance == null)
+                _instance = new LoadBalancer();
+
+            return _instance;
+        }
+
+        public void SendRequest(string request)
+        {
+            var rnd = new Random();
+            var r = rnd.Next(_servers.Count);
+            Console.WriteLine($"Request {request} sent to {_servers[r]}");
+            _requestCount += 1;
+        }
+
+        public int GetRequestCount()
+        {
+            return _requestCount;
+        }
+    }
+}
--- Cwk.DesignPatterns/Cwk.DesignPatterns.Singleton/Program.cs
+++ Cwk.DesignPatterns/Cwk.DesignPatterns.Singleton/Program.cs
@@ -6,7 +6,28 @@ namespace Cwk.DesignPatterns.Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+
+            if (b1 == b2 && b2 == b3)
+                Console.WriteLine("Same instance");
+            var balancer = LoadBalancer.GetLoadBalancer();
+
+            for (int i = 1; i < 20; i++)
+            {
+                balancer.SendRequest($"Request {i}");
+            }
+
+            var secondBalancer = LoadBalancer.GetLoadBalancer();
+
+            for (int i = 1; i < 20; i++)
+            {
+                secondBalancer.SendRequest($"Request {i}");
+            }
+
+            Console.WriteLine($"Total requests sent: {balancer.GetRequestCount()}");
+            Console.WriteLine($"Total requests sent: {secondBalancer.GetRequestCount()}");
         }
     }
 }
