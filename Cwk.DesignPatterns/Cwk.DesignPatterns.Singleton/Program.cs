81435e98c4d1dad472ae9b22b4d822d643be53d8
Dan Patrascu-Baba <dan@danpatrascu.com>
2021-04-19 20:25:29 +0300

    Initial commit

---
+++ Cwk.DesignPatterns/Cwk.DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwk.DesignPatterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
